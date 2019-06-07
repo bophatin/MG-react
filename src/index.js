77c3fa6a9ceac9122bd8699117fda10efe40bd71
Bo <bophatin@gmail.com>
2019-06-07 14:14:06 +0200

    Ajout gestion des urls avec react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+/*import * as serviceWorker from './serviceWorker'*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App'
+import Archi from './components/Archi'
+import Mode from './components/Mode'
+import PageError from './components/PageError'
+import Header from './components/Header'
+
+import './css/index.css'
 
 
-serviceWorker.unregister();
+const Root = () => (
+    <Switch>
+        <Route path='/' exact component={App} />
+        <Route path='/archi' component={Archi} />
+        <Route path='/mode' component={Mode} />
+        <Route component={PageError} />
+    </Switch>
+)
+
+
+ReactDOM.render(
+    <Router>
+        <Root />
+        <Header />
+    </Router>, document.getElementById('root')
+);
+
+/*serviceWorker.unregister();*/
