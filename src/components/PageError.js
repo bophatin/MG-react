bf61b12c32d1a7ad5e53e30107511595188be29d
Bo <bophatin@gmail.com>
2019-06-07 14:10:29 +0200

    Ajout component page 404

---
+++ src/components/PageError.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PageError extends Component {
+    render() {
+        return(
+            <h1>Error, this page does not exist</h1>
+        )
+    }
+}
+
+export default PageError;
