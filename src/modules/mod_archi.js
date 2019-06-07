9e9ccf1721926aa37853faf02c459601984a046d
Bo <bophatin@gmail.com>
2019-06-07 14:15:24 +0200

    Ajout props objet archi

---
+++ src/modules/mod_archi.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    card1 : {
+        titre : 'Rimowa',
+        image : 'https://payload.cargocollective.com/1/21/684483/13406585/RL_RIMOWA_HD_0U9A3358_670.jpg',
+    },
+    card2 : {
+        titre : 'Celestino',
+        image : 'https://i.pinimg.com/564x/5b/53/01/5b5301b1a1e6a6938108f5c0cbfdf144.jpg',
+    }
+}
